bc60845670a134b679db8e4b416b11985366be02
stanaka <shinji.tanaka@gmail.com>
2012-05-24 16:48:36 +0900

    fix typo

--- lib/fluent/plugin/out_tag_modifier_filter.rb
+++ lib/fluent/plugin/out_tag_modifier_filter.rb
@@ -1,14 +1,14 @@
 class Fluent::TagModifierFilterOutput < Fluent::Output
   Fluent::Plugin.register_output('tag_modifier', self)
 
-  config_param :behavior, :string, :default => 'replace' #れreplace or append
+  config_param :behavior, :string, :default => 'replace' # replace or append
   config_param :add_prefix, :string, :default => ''
 
   def emit(tag, es, chain)
     es.each {|time,record|
       if record['tag'] && record['tag'] != tag
         new_tag = @behavior != 'replace' ? tag + '.' : ''
-        new_tag = new_tag + @add_prefix + '.' + recode['tag']
+        new_tag = new_tag + @add_prefix + '.' + record['tag']
 
         Fluent::Engine.emit(new_tag, time, record)
       end
